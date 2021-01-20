8185567124efef7e3cf6b4ec41546574564d1fc5
KienbMi <m_kienberger@gmx.net>
2021-01-20 16:57:13 +0100

    Add entities

---
+++ CoronaTest.Core/Enums/Enums.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CoronaTest.Core
+{
+    public enum TestResult
+    {
+        Unknown,
+        Positive,
+        Negative
+    }
+
+    public enum ExaminationStates
+    {
+        New,
+        Registered,
+        Tested
+    }
+}
