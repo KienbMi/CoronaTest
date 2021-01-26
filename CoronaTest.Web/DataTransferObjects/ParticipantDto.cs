c7c2196605411a38ec943e927968b957b930168a
KienbMi <m_kienberger@gmx.net>
2021-01-26 20:11:51 +0100

    edit user data implemented

---
+++ CoronaTest.Web/DataTransferObjects/ParticipantDto.cs
@@ -0,0 +1,103 @@
+﻿using CoronaTest.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoronaTest.Web.DataTransferObjects
+{
+    public class ParticipantDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Der {0} ist verpflichtend")]
+        [DisplayName("Vorname")]
+        public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Der {0} ist verpflichtend")]
+        [DisplayName("Nachname")]
+        public string Lastname { get; set; }
+
+        [Required(ErrorMessage = "Der {0} ist verpflichtend")]
+        [DisplayName("Geburtstag")]
+        public DateTime Birthdate { get; set; }
+
+        [Required(ErrorMessage = "Das {0} ist verpflichtend")]
+        [DisplayName("Geschlecht")]
+        public string Gender { get; set; }
+
+        [Required(ErrorMessage = "Die {0} ist verpflichtend")]
+        [StringLength(10, ErrorMessage = "Die {0} muss genau 10 Zeichen lang sein!", MinimumLength = 10)]
+        [DisplayName("Sozialversicherungsnummer")]
+        public string SocialSecurityNumber { get; set; }
+
+        [Required(ErrorMessage = "Die {0} ist verpflichtend")]
+        [StringLength(16, ErrorMessage = "Die {0} muss zw. {1} und {2} Zeichen lang sein!", MinimumLength = 5)]
+        [DisplayName("Handynummer")]
+        public string Mobilephone { get; set; }
+
+        [Required(ErrorMessage = "Die {0} ist verpflichtend")]
+        [DisplayName("Straße")]
+        public string Street { get; set; }
+
+        [Required(ErrorMessage = "Die {0} ist verpflichtend")]
+        [DisplayName("Hausnummer")]
+        public string HouseNumber { get; set; }
+
+        [DisplayName("Stiege")]
+        public string Stair { get; set; }
+
+        [DisplayName("Tür")]
+        public string Door { get; set; }
+
+        [Required(ErrorMessage = "Die {0} ist verpflichtend")]
+        [DisplayName("PLZ")]
+        public string Postalcode { get; set; }
+
+        [Required(ErrorMessage = "Die {0} ist verpflichtend")]
+        [DisplayName("Ort")]
+        public string City { get; set; }
+
+        public ParticipantDto()
+        {
+
+        }
+
+        public ParticipantDto(Participant participant)
+        {
+            Id = participant.Id;
+            Firstname = participant.Firstname;
+            Lastname = participant.Lastname;
+            Birthdate = participant.Birthdate;
+            Gender = participant.Gender;
+            SocialSecurityNumber = participant.SocialSecurityNumber;
+            Mobilephone = participant.Mobilephone;
+            Street = participant.Street;
+            HouseNumber = participant.HouseNumber;
+            Stair = participant.Stair;
+            Door = participant.Door;
+            Postalcode = participant.Postalcode;
+            City = participant.City;
+        }
+
+        public void CopyContentToModel(ref Participant model)
+        {
+            model.Id = Id;
+            model.Firstname = Firstname;
+            model.Lastname = Lastname;
+            model.Birthdate = Birthdate;
+            model.Gender = Gender;
+            model.SocialSecurityNumber = SocialSecurityNumber;
+            model.Mobilephone = Mobilephone;
+            model.Street = Street;
+            model.HouseNumber = HouseNumber;
+            model.Stair = Stair;
+            model.Door = Door;
+            model.Postalcode = Postalcode;
+            model.City = City;           
+        }
+    }
+}
--- CoronaTest.Web/Pages/User/Details.cshtml.cs
+++ CoronaTest.Web/Pages/User/Details.cshtml.cs
@@ -1,16 +1,76 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CoronaTest.Core.Contracts;
+using CoronaTest.Core.Models;
+using CoronaTest.Core.Validations;
+using CoronaTest.Web.DataTransferObjects;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace CoronaTest.Web.Pages.User
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly IUnitOfWork _unitOfWork;
+        [BindProperty]
+        public Guid VerificationIdentifier { get; set; }
+        [BindProperty]
+        public ParticipantDto Participant { get; set; }
+
+        public DetailsModel(IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
+        }
+        
+        public async Task<IActionResult> OnGetAsync(Guid? verificationIdentifier)
+        {
+            if (verificationIdentifier == null)
+            {
+                return RedirectToPage("/Security/TokenError");
+            }
+
+            VerificationToken verificationToken = await _unitOfWork.VerificationTokens.GetTokenByIdentifierAsync(verificationIdentifier.Value);
+
+            if (verificationToken?.Identifier != verificationIdentifier || verificationToken.ValidUntil < DateTime.Now)
+            {
+                return RedirectToPage("/Security/TokenError");
+            }
+
+            VerificationIdentifier = verificationToken.Identifier;
+            var participant = verificationToken.Participant;
+            Participant = new ParticipantDto(participant);
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            if (!SSNrValidation.CheckSsnr(Participant.SocialSecurityNumber))
+            {
+                ModelState.AddModelError($"{nameof(Participant)}.{nameof(Participant.SocialSecurityNumber)}", $"Die SVNr {Participant.SocialSecurityNumber} ist ungültig");
+                return Page();
+            }
+
+            var participantInDb = await _unitOfWork.Participants.GetByIdAsync(Participant.Id);
+            Participant.CopyContentToModel(ref participantInDb);
+
+            try
+            {
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (ValidationException ex)
+            {
+                ModelState.AddModelError("", $"{ex.Message}");
+                return Page();
+            }
+
+            return RedirectToPage("/User/Index", new { verificationIdentifier = VerificationIdentifier } );
         }
     }
 }
