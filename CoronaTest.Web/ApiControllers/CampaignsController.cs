64423c92c01cada97b67f953ae265af00e180b59
KienbMi <m_kienberger@gmx.net>
2021-02-23 23:52:52 +0100

    add first controller and swagger

---
+++ CoronaTest.Web/ApiControllers/CampaignsController.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using CoronaTest.Core.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SmartSchool.Web.ApiControllers
+{
+    /// <summary>
+    /// API-Controller für die Abfrage von Mitgliedern
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CampaignsController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        /// <summary>
+        /// Constructor mit DI
+        /// </summary>
+        /// <param name="unitOfWork"></param>
+        public CampaignsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        /// <summary>
+        /// Liefert alle Namen der Sensoren
+        /// </summary>
+        /// <response code="200">Die Abfrage war erfolgreich.</response>
+        // GET: api/Campaigns
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<string>> GetCampaignNames()
+        {
+            //Sensor[] sensors = await _unitOfWork.SensorRepository.GetAllAsync();
+            return "Das ist der erste Test";
+        }
+    }
+}
--- CoronaTest.Web/Startup.cs
+++ CoronaTest.Web/Startup.cs
@@ -7,10 +7,13 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using StringRandomizer;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace CoroneTest.Web
@@ -36,6 +39,32 @@ namespace CoroneTest.Web
             services.AddSingleton<Randomizer>(_ => new Randomizer());
 
             services.AddRazorPages();
+            services.AddControllers();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "CoronaTest API",
+                    Description = "Die API liefert alle wichtigen Infos zu den Corona Tests",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Michael Kienberger",
+                        Email = "m_kienberger@gmx.net"
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT"
+                    }
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -62,6 +91,14 @@ namespace CoroneTest.Web
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "CoronaTest API V1");
+                c.DisplayRequestDuration();
             });
         }
     }
